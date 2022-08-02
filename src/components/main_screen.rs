2a42eff59d1ffdff42c589c79435929678a14b4e
Raforawesome <ubuntuhasnoroblox@gmail.com>
2022-08-02 18:55:48 -0400

    Added button on_click effects

--- src/components/main_screen.rs
+++ src/components/main_screen.rs
@@ -10,5 +10,13 @@ pub fn MainScreen(cx: Scope<AppProps>) -> Element {
 			height: "100px",
 			class: "logo"
 		}
+		input { style: "margin-top:12vh;", placeholder: "Username" }
+		input { "type": "password", style: "margin-top:3vh;", placeholder: "Password" }
+		button {
+			onclick: move |_| {
+				println!("Log in button clicked")
+			},
+			"Log In"
+		}
 	))
 }
