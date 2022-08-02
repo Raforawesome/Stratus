c5a62842f73223276df4d0e326d5b53eef4fa230
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-08-02 17:45:51 -0400

    Updated component format to no longer use macros

--- src/components/main_screen.rs
+++ src/components/main_screen.rs
@@ -1,13 +1,14 @@
-#[macro_export]
-macro_rules! main_screen_component {
-	() => {
-		rsx!(
-			style { [include_str!("./css/main_screen.css")] }
-			img { 
-				src: include_png!("./assets/images/logo-full.png"),
-				height: "100px",
-				class: "logo"
-			}
-		)
-	}
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+use crate::{ AppProps, include_png };
+
+pub fn MainScreen(cx: Scope<AppProps>) -> Element {
+	cx.render(rsx!(
+		style { [include_str!("../css/main_screen.css")] }
+		img { 
+			src: include_png!("../assets/images/logo-full.png"),
+			height: "100px",
+			class: "logo"
+		}
+	))
 }
