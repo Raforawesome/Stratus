b6ef93b125d5ca5a7ff0eecd285dbb020f4a6e19
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-09-27 09:29:14 -0400

    Added error text shown on unsuccessful login

--- src/components/login_screen.rs
+++ src/components/login_screen.rs
@@ -4,6 +4,8 @@ use dioxus::prelude::*;
 
 pub fn LoginScreen(cx: Scope) -> Element {
     let password_input = use_state(&cx, || "".to_string());
+    let show_error = use_state(&cx, || true);
+    let to_show_error: String = (*show_error.get()).to_string();
 
     cx.render(rsx!(
         style { [include_str!("../css/login_screen.css")] }
@@ -25,15 +27,22 @@ pub fn LoginScreen(cx: Scope) -> Element {
         button {
             onclick: move |_| {
                 if login::try_login(password_input.get()) {
-                    println!("Successful login");
+                    // println!("Successful login");
                     let router = use_router(&cx);
                     router.replace_route("/dash", None, None);
+                    show_error.set(true);
                 } else {
-                    println!("Unsuccessful login");
+                    // println!("Unsuccessful login");
+                    show_error.set(false);
                     // Include error shake & message here
                 }
             },
             "Unlock"
+        }
+        p {
+            class: "error-text",
+            hidden: "{to_show_error}",
+            "Incorrect password!"
         }
     ))
 }
