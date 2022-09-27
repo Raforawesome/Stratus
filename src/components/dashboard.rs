acf9170be830621d9688fcdb3398687ebdc613b7
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-09-27 14:23:31 -0400

    Create stylesheet for dashboard screen

--- src/components/dashboard.rs
+++ src/components/dashboard.rs
@@ -1,9 +1,12 @@
 #![allow(non_snake_case)]
+use crate::functions::login;
 use dioxus::prelude::*;
 // use crate::include_png;
 
 pub fn Dashboard(cx: Scope) -> Element {
+    let name: String = login::get_name();
     cx.render(rsx!(
-        h1 { "Dashboard" }
+        style { [include_str!("../css/dashboard.css")] }
+        h1 { class: "title", "Welcome back, {name}!" }
     ))
 }
